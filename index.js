45764915e5273b02e18829f32406b323aa403f99
Rocky Essel <rockyessel76@gmail.com>
2023-09-09 19:57:47 +0000

    refactored

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (request, response)=>{
-  response.status(200).send('System Good!')
-})
+app.get('/', (request, response) => {
+  response.status(200).send('System Good!');
+});
 
 app.use('/api/v1/files', require('./routes/files'));
 
