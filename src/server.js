5a017cca83688b5d51279ec46152025196e0abd3
Rocky Essel <rockyessel76@gmail.com>
2023-09-09 19:12:20 +0000

    completed

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', async (request, response)=>{
+app.get('/', (request, response)=>{
   response.status(200).send('System Good!')
 })
 
