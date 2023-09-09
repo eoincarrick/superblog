6ebde5f40a3c38d4ce3d6199fbb3bc09f8bd727a
Rocky Essel <rockyessel76@gmail.com>
2023-09-09 20:27:19 +0000

    cec

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const PORT = process.env.PORT || 8000;
+const path = require('path');
+
+const server = express();
+dotenv.config({ path: '.env' });
+
+server.use(morgan('dev'));
+server.use(cors());
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+server.use(express.static(path.join(__dirname, 'public')));
+
+server.get('/', (request, response) => {
+  response.status(200).send('System Good!');
+});
+
+server.use('/api/v1/files', require('./routes/files'));
+
+server.listen(PORT, () =>
+  console.log(`Server running on: http://localhost:${PORT}`)
+);
+
+module.exports = server;
